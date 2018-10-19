75914f0bb55947db4bfa3449fd5ef165396c0648
Allan Hartigan <allan.hartigan@gmail.com>
2018-10-19 19:10:27 -0400

    Created scheme and seeds file.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Mushroom Burger', false);
+INSERT INTO cats (burger_name, sleepy) VALUES ('Bacon Double Cheeseburger', false);
+INSERT INTO cats (burger_name, sleepy) VALUES ('Chipotle Burger', false);
